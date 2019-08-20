0dcd9766d947e7b10e87789d1ebc7086b085dbeb
Gustavo Murayama <gmurayama@outlook.com>
2019-08-20 00:28:03 -0300

    Added AST for math operations

--- src/c_parser.rs
+++ src/c_parser.rs
@@ -1,4 +1,4 @@
-use crate::ast_parser::AstNode;
+use crate::ast_parser::{ AstNode, ValueType, Expr };
 
 pub struct CParser;
 
@@ -25,6 +25,7 @@ impl CParser {
 
                 format!("
                     #include <stdio.h>
+                    #include <math.h>
                     int main() {{
                         {b}
                         return 0;
@@ -34,13 +35,46 @@ impl CParser {
                 let mut declarations = String::new();
 
                 for var in variables {
-                    match &var.value {
-                        Some(value) => declarations.push_str(&format!("{} {}={};\n", var.var_type, var.ident, value)),
+                    match var.value {
+                        Some(value) => declarations.push_str(&format!("{} {}={};\n", var.var_type, var.ident, CParser::parse_variable_value(value))),
                         None => declarations.push_str(&format!("{} {};\n", var.var_type, var.ident))
                     };
                 };
 
                 return declarations;
+            }
+        }
+    }
+
+    fn parse_variable_value(value: ValueType) -> String {
+        match value {
+            ValueType::Str(v) => v,
+            ValueType::Float(v) => format!("{}", v),
+            ValueType::Ident(v) => format!("{}", v),
+            ValueType::Expression(expr) => {
+                let mut parsed_expr = String::new();
+
+                for e in expr {
+                    let parsed = match e {
+                        Expr::Add => String::from("+"),
+                        Expr::Divide => String::from("+"),
+                        Expr::Multiply => String::from("+"),
+                        Expr::Subtract => String::from("+"),
+                        Expr::Power { base, exp } => {
+                            let base = CParser::parse_variable_value(*base);
+                            let exp = CParser::parse_variable_value(*exp);
+                            format!("pow({}, {})", base, exp)
+                        }
+                        Expr::Term(expr) => {
+                            let parsed_expr = CParser::parse_variable_value(*expr);
+                            format!("{}", parsed_expr)
+                        }
+                    };
+
+                    parsed_expr.push_str(&parsed);
+                }
+
+                return format!("({})", parsed_expr);
             }
         }
     }
