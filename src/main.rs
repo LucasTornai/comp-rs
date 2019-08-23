4eaf53cd14cbf5306d93b5fcf123625ee80add1b
lucastornai <lucas@tornai.dev>
2019-08-23 18:01:37 -0300

    Now also output C program to out.c

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,17 @@ mod ast_parser;
 use c_parser::CParser;
 use ast_parser::AstParser;
 use std::env;
+use std::fs::File;
+use std::io::Write;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let path = env::args().nth(1).unwrap();
     
     let ast = AstParser::parse(&path);
     let parsed_to_c = CParser::parse(ast);
     println!("{}", parsed_to_c);
+
+    let mut output_file = File::create("out.c")?;
+    output_file.write_all(parsed_to_c.as_bytes())?;
+    Ok(())
 }
